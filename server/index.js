fc94fe3de5ef0b70bad150048693d0050bbc5456
mrkhan-dev <sk5094625@gmail.com>
2024-10-19 23:48:13 +0600

    delete functionality working successful

--- server/index.js
+++ server/index.js
@@ -104,6 +104,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a room
+    app.delete("/room/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await roomsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // get single room
     app.get("/room/:id", async (req, res) => {
       const id = req.params.id;
